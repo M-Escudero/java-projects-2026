08c86923ce7bde837c3f6659e0cf08ab86692991
Miguel Angel Escudero <miguelangelescuderojimenez@gmail.com>
2026-06-26 19:23:02 +0200

    feat(semana1): migrar a ArrayList y bucles for/while

--- semana1-gestor-tareas/gestor-tareas/src/main/java/Main.java
+++ semana1-gestor-tareas/gestor-tareas/src/main/java/Main.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,13 +13,29 @@ public class Main {
         boolean activo = true;
 
         // --- Array de tareas ---
-        String [] tareas = {
+     /*   String [] tareas = {
                 "Sacar a pasear a Kira",
                 "Ir al gimnasio",
                 "Estudiar Java",
                 "Ver el Mundial",
                 "Respetar la dieta"
-        };
+        }; */
+
+        // --- Sustitución de Array por Arraylist ---
+        List<String> tareas = new ArrayList<>(List.of(
+                "Sacar a pasear a Kira",
+                "Ir al gimnasio",
+                "Estudiar Java",
+                "Ver el Mundial",
+                "Respetar la dieta"
+        ));
+
+        // --- Añadir nuevas tareas ---
+        tareas.add("Leer 30 minutos");
+        tareas.add("Revisar el correo");
+
+        // --- Eliminar la tarea en posición 0
+        tareas.remove(0);
 
         // --- Imprimir las variables ---
         System.out.println("Nombre: " + nombre);
@@ -26,15 +45,27 @@ public class Main {
 
         // --- Recorrer el array e imprimir las tareas numeradas ---
         System.out.println("\n --- Lista de tareas ---");
-        for (int i = 0; i < tareas.length; i++){
-            System.out.println((i+1) + ". " + tareas[i]);
+        for (int i = 0; i < tareas.size(); i++){
+            System.out.println((i+1) + ". " + tareas.get(i));
         }
 
+        // --- Contar tareas que contienen una palabra ---
+        int contador = 0;
+        int i = 0;
+        while (i < tareas.size()){
+            if (tareas.get(i).contains("Java")){
+                contador++;
+            }
+            i++;
+        }
+        System.out.println("Tareas que contienen 'Java': " + contador);
+
+
         // --- Condicional ---
-        if (tareas.length == 0){
+        if (tareas.size() == 0){
             System.out.println("No hay tareas pendientes.");
         } else {
-            System.out.println("\nTienes " + tareas.length + " tareas pendientes.");
+            System.out.println("\nTienes " + tareas.size() + " tareas pendientes.");
         }
 
 
