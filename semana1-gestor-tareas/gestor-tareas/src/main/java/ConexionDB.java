b5af742a805bc4bf1c8ba790b8344f4b6be4fdcd
Miguel Angel Escudero <miguelangelescuderojimenez@gmail.com>
2026-07-21 01:20:23 +0200

    fix(seguridad): leer contraseña de BD desde variable de entorno

--- semana1-gestor-tareas/gestor-tareas/src/main/java/ConexionDB.java
+++ semana1-gestor-tareas/gestor-tareas/src/main/java/ConexionDB.java
@@ -6,7 +6,7 @@ public class ConexionDB {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/bootcamp";
     private static final String NOMBRE = "postgres";
-    private static final String PASSWORD = "3778";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConexion() throws SQLException{
         return DriverManager.getConnection(URL,NOMBRE, PASSWORD);
